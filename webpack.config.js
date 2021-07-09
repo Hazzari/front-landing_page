c24c9abfa1e5f3c398ccc98334fc5e09fc5d398f
Aleksan <Dev.aleksan@gmail.com>
2021-07-09 12:07:47 +0300

    feat: Add section userful and progress

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,12 @@ module.exports = {
                 {
                     from: path.resolve(__dirname, 'src/images'),
                     to: path.resolve(__dirname, 'dist/images')
-                }
+                },
+                              {
+                    from: path.resolve(__dirname, 'src/video'),
+                    to: path.resolve(__dirname, 'dist/video')
+                },
+
             ]
         })
     ],
