bb23c0d311b699083cc22d72ec313fafa331daea
Aleksan <Dev.aleksan@gmail.com>
2021-07-10 02:06:07 +0300

    feat: Add section services and comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,29 @@
 import 'bootstrap/dist/css/bootstrap-reboot.css';
 import '../css/style.css';
 
+import UI from "./config/ui.config";
 
+const {tabs,} = UI;
+window.addEventListener("DOMContentLoaded", () => {
+
+// events
+    tabs.forEach(elem => {
+        onClick(elem);
+    });
+
+// handlers
+
+    function onClick(elem) {
+        elem.addEventListener("click", () => {
+            tabs.forEach(otherElem => {
+                if (otherElem.classList.contains("tabs__item_active")) {
+                    otherElem.classList.remove("tabs__item_active");
+                }
+            });
+
+            elem.classList.add("tabs__item_active");
+
+        });
+
+    }
+});
---
+++ src/js/config/ui.config.js
@@ -0,0 +1,6 @@
+const UI = {
+    tabs: document.querySelectorAll(".tabs__item"),
+};
+
+export default UI;
+
